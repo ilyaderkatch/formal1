4ba4e96b87dd8f23e3c3edc9ebc02d0262681068
ilyaderkatch <47671444+ilyaderkatch@users.noreply.github.com>
2019-11-07 04:13:28 +0300

    fixed bug, added tests and wrote description

--- test.py
+++ test.py
@@ -7,6 +7,8 @@ def test_basic():
     assert main.find_answer(['acb..bab.c.*.ab.ba.+.+*a.', '4', '0']) == 'YES'  # (acb + b(abc)*(ab + ba))*a
     assert main.find_answer(['acb..bab.c.*.ab.ba.+.+*a.', '5', '0']) == 'YES'  # (acb + b(abc)*(ab + ba))*a
     assert main.find_answer(['acb..bab.c.*.ab.ba.+.+*a.', '14', '13']) == 'YES'  # (acb + b(abc)*(ab + ba))*a
+    assert main.find_answer(['abc..*', '9', '2']) == 'NO' # (abc)*
+    assert main.find_answer(['ab.*cab..*.', '5', '1']) == 'YES' # (ab)*(cde)*
 
 def test_error_a_lot_of_letters():
     assert main.find_answer(['aaa+', '5', '1']) == 'ERROR'
@@ -17,4 +19,6 @@ def test_error_a_lot_of_operators():
     assert main.find_answer(['a+a', '3', '2']) == 'ERROR'
     assert main.find_answer(['a*b*c*1*++++*', '5', '1']) == 'ERROR'
 
-
+def test_error_new_letters():
+    assert main.find_answer(['ad+', '3', '2']) == 'ERROR'
+    assert main.find_answer(['Ab+*', '3', '2']) == 'ERROR'
